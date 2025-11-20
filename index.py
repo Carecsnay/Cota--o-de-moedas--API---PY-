899a78dd92bfceef0ca09837d8e1c0a2cacfa101
Bruno Desktop <carecsnay1@gmail.com>
2025-11-20 20:59:52 -0300

    feat: add currency conversion script in python feat: add readme file with project description

---
+++ index.py
@@ -0,0 +1,64 @@
+import requests
+
+def get_cotacao(nomeclaturaDestino):
+    url = 'https://api.exchangerate-api.com/v4/latest/' + nomeclaturaDestino
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code==200:
+        return data['rates']
+    else:
+        print('⚠️ Erro ao obter cotações!', response.status_code)
+        return None
+        
+def converterCotacao(nomeclaturaOrigem, nomeclaturaDestino, valor): 
+    rates = get_cotacao(nomeclaturaOrigem) 
+    if rates:
+        taxa_conversao = rates[nomeclaturaDestino]
+        valor_convertido = round(valor * taxa_conversao, 4)
+        return f'{valor} {nomeclaturaOrigem} equivale(m) a {valor_convertido} {nomeclaturaDestino}.'
+    return 'Não foi possível obter a cotação.'
+
+
+
+def menu():
+    print()
+    print('1 - Converter Dolar em Real')
+    print('2 - Converter Ienes em Real')
+    print('3 - Converter Euros em Real')
+    print('4 - Eu quero escolher')
+    print('0 - Sair!')
+    print()
+
+opcao = 1
+
+while opcao != 0:
+    menu()
+    opcao = int(input('Escolha uma opção: '))
+    
+    match opcao: 
+        case 1:
+            valor = input('Valor a ser convertido? ')
+            valor = float(valor)
+            print(converterCotacao('USD','BRL', valor))
+
+        case 2:
+            valor = input('Valor a ser convertido? ')
+            valor = float(valor)
+            print(converterCotacao('JPY','BRL', valor))
+
+        case 3:
+            valor = input('Valor a ser convertido? ')
+            valor = float(valor)
+            print(converterCotacao('EUR','BRL', valor))
+
+        case 4:
+            origem = str(input('Digite a nomeclatura da ORIGEM: '))
+            destino = str(input('Digite a nomeclatura do DESTINO: '))
+            valor = float(input('Digite o VALOR a ser comparado: '))
+
+            resultado = converterCotacao(origem, destino, valor)
+            print(resultado)
+
+        case 0:
+            exit
