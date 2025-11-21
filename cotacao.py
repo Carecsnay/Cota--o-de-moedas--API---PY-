f80d36b69d0617e6ce79c727548833c35701d123
Bruno Desktop <carecsnay1@gmail.com>
2025-11-20 21:06:32 -0300

    feat: extract currency conversion logic into cotacao.py feat: add .gitignore file to ignore __pycache__ refactor: remove redundant code from index.py and import converterCotacao from cotacao.py

---
+++ cotacao.py
@@ -0,0 +1,21 @@
+import requests
+
+def get_cotacao(nomeclaturaDestino):
+    url = 'https://api.exchangerate-api.com/v4/latest/' + nomeclaturaDestino
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code==200:
+        return data['rates']
+    else:
+        print('⚠️ Erro ao obter cotações!', response.status_code)
+        return None
+        
+def converterCotacao(nomeclaturaOrigem, nomeclaturaDestino, valor): 
+    rates = get_cotacao(nomeclaturaOrigem) 
+    if rates:
+        taxa_conversao = rates[nomeclaturaDestino]
+        valor_convertido = round(valor * taxa_conversao, 4)
+        return f'{valor} {nomeclaturaOrigem} equivale(m) a {valor_convertido} {nomeclaturaDestino}.'
+    return 'Não foi possível obter a cotação.'
+
